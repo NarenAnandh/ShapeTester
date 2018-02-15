6fef345866b5fb943d4976a29a96c6ff2c41327a
Naren Anandh <Narenanandh01@gmail.com>
2018-02-15 07:54:31 -0700

    Add files via upload

---
+++ ShapeTester/Pyramid.java
@@ -0,0 +1,40 @@
+public class Pyramid{
+  private float length;
+  private float height;
+  private float width;
+
+  public Pyramid() {
+    this.setHeight(0);
+    this.setLength(0);
+    this.setWidth(0);
+  }
+  public float getLength(){
+    return length;
+  }
+  public void setLength (float length){
+    this.length = length;
+  }
+  public float getHeight(){
+    return height;
+  }
+  public void setHeight(float height){
+    this.height = height;
+  }
+  public float getWidth(){
+    return width;
+  }
+  public void setWidth(float width){
+    this.width = width;
+  }
+  public float getVolume() {
+    double mat = (height*length*width);
+    return (float) mat*1/3;
+  }
+  public float getSurfaceArea(){
+    double xi = (Math.sqrt((this.width/2)*(this.width/2)+(this.height)*(this.height)));
+    double zi = (Math.sqrt((this.length/2)*(this.length/2)+(this.height)*(this.height)));
+    double sat = (this.length * this.width + this.length* xi + this.width * zi);
+    return (float) sat;
+  }
+}
+//2lw+2lh+2wh
